406da07878f1081e0b36ede19d37286b0191bd5e
Manoj kumar <manoj166174@gmail.com>
2025-04-18 22:22:44 +0530

    Create bank.java

---
+++ bank.java
@@ -0,0 +1,16 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class Main {
+    public static void main(String[] args) {
+       int deposit  = 1;
+       int widraw = 2;
+        if(deposit==1){
+            System.out.println("potu thola da eruma maadu");
+        }else if(widraw==2){
+             System.out.println("poda punda");
+        }else{
+             System.out.println("eduthu thola");
+        }
+    }
+}
