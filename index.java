6a6063decf945d0257956238d1f7dafcb33be66b
Manoj kumar <manoj166174@gmail.com>
2025-04-18 22:22:03 +0530

    Create index.java

---
+++ index.java
@@ -0,0 +1,11 @@
+
+class Main {
+    public static void main(String[] args) {
+       int num = 19;
+        if(num%2==0 && num%3==0){
+            System.out.println("not prime");
+        }else{
+               System.out.println("prime");
+        }
+    }
+}
